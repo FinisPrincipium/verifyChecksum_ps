23fce1f518c05dfc6d81b9d4b9425727831d6a7d
Steven R <mitchelramsay@gmail.com>
2018-06-14 19:03:42 -0400

    Adding main file

---
+++ verifyChecksum_ps.ps1
@@ -0,0 +1,14 @@
+﻿#path to file & hash
+$FILE = Read-Host -Prompt 'File Path';
+$HASH = Read-Host -Prompt 'Enter checksum';
+$FILEHASHED = Get-FileHash $FILE
+if ($FILEHASHED -match $HASH){
+write-host "Hash matched"
+}
+else{
+write-host "Hash currupt"
+}
+write-host $FILEHASHED
+write-host $HASH
+Read-Host -prompt 'Press enter to close'
+
